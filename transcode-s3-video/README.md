f3a6a3c4bc1748415c0f601ce6f64a80f977a3a9
Sawyer Hollenshead <s.hollenshead@gmail.com>
2016-06-25 15:15:45 -0400

    Use new callback method available to 4.3 runtime

--- transcode-s3-video/README.md
+++ transcode-s3-video/README.md
@@ -1,6 +1,6 @@
 ## Usage
 
 1. [Create an Elastic Transcoder Pipeline](http://docs.aws.amazon.com/elastictranscoder/latest/developerguide/creating-pipelines.html)
-2. Configure the variables in `lambda.js`: Set your Pipeline ID, presets, and output directory.
+2. Set your Pipeline ID, presets, and output directory in the `CONFIG` object at the top of `lambda.js`.
 3. [Create a new Lambda function](http://docs.aws.amazon.com/lambda/latest/dg/getting-started.html). Skip the blueprints screen, copy + paste the contents of `lambda.js` into the inline code editor. Make sure the "Handler" is set to index.handler. Make sure the IAM role you select has the `elastictranscoder:CreateJob` permission. (see policy.json)
 4. Set up your S3 bucket to [send events to your Lambda function](http://docs.aws.amazon.com/AmazonS3/latest/dev/NotificationHowTo.html) when a new video object is created.
--- transcode-s3-video/lambda.js
+++ transcode-s3-video/lambda.js
@@ -1,20 +1,21 @@
-// Your AWS Elastic Transcoder Pipeline:
-var PIPELINE_ID = "";
-
-// Set which presets should be used to transcode
-// your video. Add a new object to this array
-// for each preset. The keySuffix will be appended
-// to the transcoded file's name and should at least
-// contain the expected file extension:
-var PRESETS = [{
-  id: "1351620000001-100070", // "System preset: Web"
-  keySuffix: ".mp4"
-}];
-
-// Set the directory where you'd like transcoded
-// videos to be placed. This can also be used to
-// add a prefix to the transcoded file's name:
-var OUTPUT_KEY_PREFIX = "uploads/";
+'use strict';
+const CONFIG = {
+  // Your AWS Elastic Transcoder Pipeline:
+  PIPELINE_ID: "",
+  // Set which presets should be used to transcode
+  // your video. Add a new object to this array
+  // for each preset. The keySuffix will be appended
+  // to the transcoded file's name and should at least
+  // contain the expected file extension:
+  PRESETS: [{
+    id: "1351620000001-100070", // "System preset: Web"
+    keySuffix: ".mp4"
+  }],
+  // Set the directory where you'd like transcoded
+  // videos to be placed. This can also be used to
+  // add a prefix to the transcoded file's name:
+  OUTPUT_KEY_PREFIX: "uploads/"
+};
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // DO NOT EDIT BELOW THIS LINE UNLESS YOU'RE WISE
@@ -27,7 +28,7 @@ function basename(path) {
 }
 
 function outputs(name) {
-  return PRESETS.map(function(output) {
+  return CONFIG.PRESETS.map(function(output) {
     return {
       Key: name + output.keySuffix,
       PresetId: output.id,
@@ -36,24 +37,22 @@ function outputs(name) {
   });
 }
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
   console.log('Received event:', JSON.stringify(event, null, 2));
-  var key = event.Records[0].s3.object.key;
-  var params = {
+  let key = event.Records[0].s3.object.key;
+  let params = {
     Input: {
       Key: key
     },
-    PipelineId: PIPELINE_ID,
-    OutputKeyPrefix: OUTPUT_KEY_PREFIX,
+    PipelineId: CONFIG.PIPELINE_ID,
+    OutputKeyPrefix: CONFIG.OUTPUT_KEY_PREFIX,
     Outputs: outputs(basename(key))
   };
 
   elastictranscoder.createJob(params, function(err, data) {
-    if (err){
-      console.log(err, err.stack);
-      context.fail();
-      return;
-    }
-    context.succeed();
+    if (err)
+      return callback(err);
+
+    return callback(null, data);
   });
 };
