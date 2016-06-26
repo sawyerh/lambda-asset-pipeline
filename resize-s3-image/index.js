eb90f0e660233226dca87bc8bffc4e70b0fb6e51
Sawyer Hollenshead <s.hollenshead@gmail.com>
2016-06-25 22:06:11 -0400

    Working demo of image resizer

---
+++ resize-s3-image/index.js
@@ -0,0 +1,139 @@
+'use strict';
+const CONFIG = {
+  OUTPUT_PREFIX: 'uploads/',
+  SIZES: [
+    {
+      maxHeight: 100,
+      maxWidth: 100
+    }
+  ]
+};
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// DO NOT EDIT BELOW THIS LINE UNLESS YOU'RE WISE
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var AWS = require('aws-sdk');
+var ImageMagick = require("imagemagick");
+var fs = require('fs');
+var s3 = new AWS.S3();
+
+function basename(path) {
+  return path.split('/').reverse()[0].split('.')[0];
+}
+
+function extension(path) {
+  let typeMatch = path.match(/\.([^.]*)$/);
+  if (!typeMatch) return null;
+  return typeMatch[1];
+}
+
+function ImageResizer(bucket, key, callback) {
+  this.bucket = bucket;
+  this.key = key;
+  this.ext = extension(key);
+  this.callback = callback;
+  this.s3Object = null;
+  this.width = null;
+  this.height = null;
+}
+
+ImageResizer.prototype.resize = function() {
+  this.download()
+    .then(this.getDimensions.bind(this))
+    .then(this.transformAndUpload.bind(this))
+    .then(() => this.callback(null, `Resized ${this.key}`))
+    .catch((error) => this.callback(error));
+};
+
+ImageResizer.prototype.download = function() {
+  return new Promise((resolve, reject) => {
+    s3.getObject({ Bucket: this.bucket, Key: this.key }, (error, s3Object) => {
+      if (error) return reject(error);
+      this.s3Object = s3Object;
+      resolve();
+    });
+  });
+};
+
+ImageResizer.prototype.getDimensions = function() {
+  return new Promise((resolve, reject) => {
+    const tmpFilePath = `/tmp/inputFile.${this.ext}`;
+    fs.writeFileSync(tmpFilePath, this.s3Object.Body);
+
+    ImageMagick.identify(['-format', '%wx%h', tmpFilePath], (err, output) => {
+      fs.unlinkSync(tmpFilePath);
+      if (err) return reject(err);
+
+      let dimensions = output.trim().split('x');
+      this.width = dimensions[0];
+      this.height = dimensions[1];
+      resolve();
+    });
+  });
+};
+
+ImageResizer.prototype.transformAndUpload = function() {
+  return new Promise((resolve, reject) => {
+    // Infer the scaling factor to avoid
+    // stretching the image unnaturally.
+    const scalingFactor = Math.min(
+      CONFIG.SIZES[0].maxWidth / this.width,
+      CONFIG.SIZES[0].maxHeight / this.height
+    );
+
+    const tmpFilePath = `/tmp/resized.${this.ext}`;
+    const width  = scalingFactor * this.width;
+    const height = scalingFactor * this.height;
+    const data = {
+      srcData: this.s3Object.Body,
+      dstPath: tmpFilePath,
+      height: height,
+      width: width
+    };
+
+    try {
+      ImageMagick.resize(data, (err, stdout, stderr) => {
+        if (err) return reject(err);
+        var resizedBuffer = new Buffer(fs.readFileSync(tmpFilePath));
+        this.upload(resizedBuffer)
+          .then(() => {
+            try { fs.unlinkSync(tmpFilePath) } catch (e) {}
+            resolve();
+          })
+          .catch(reject);
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+ImageResizer.prototype.upload = function(resizedBuffer) {
+  const filename = basename(this.key) + "_" + this.width + "x" + this.height + "." + this.ext;
+  console.log(filename);
+
+  return new Promise((resolve, reject) => {
+    s3.putObject({
+      Bucket: this.bucket,
+      Key: `${CONFIG.OUTPUT_PREFIX}${filename}`,
+      Body: resizedBuffer,
+      ContentType: this.s3Object.ContentType
+    }, (err) => {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
+};
+
+exports.handler = function(event, context, callback) {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+  const ext = extension(key);
+  let resizer = new ImageResizer(bucket, key, callback);
+
+  if (!ext || ['jpg', 'jpeg', 'png'].indexOf(ext) < 0)
+    return callback(null, "Skipping transformation. File extension is not jpg/jpeg/png: " + key);
+
+  resizer.resize();
+};
